211e608aaa6de67fb2d725eed0b50640b8a45a98
Rp7rf <westhouse51@gmail.com>
2019-02-01 00:18:58 +0900

    initial commit

---
+++ abc114_a.cpp
@@ -0,0 +1,16 @@
+// comment
+// #input #output
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+  int n;
+  cin>>n;
+  if( n == 3 || n == 5 || n == 7 ) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+}
