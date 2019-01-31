33f231c67bb585613b9a9c359360ab122dceae62
Rp7rf <westhouse51@gmail.com>
2019-02-01 01:00:54 +0900

    solved abc072_b

---
+++ abc072_b.cpp
@@ -0,0 +1,13 @@
+// comment
+// #input #output
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+  string s;
+  cin>>s;
+  for(int i = 0 ; i < s.size() ; i += 2)cout<<s[i];
+    cout<<endl;
+}
